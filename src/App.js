d4fdc8dacb14a84344e15d40bd3d5a976d13b98a
G4MB1T24 <65242995+G4MB1T24@users.noreply.github.com>
2022-09-03 15:25:53 +0530

    Added allANime fetchpoint KINDA havent implement to load data in chunks , it is buggy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Home from "./Components/Home";
 import Nav from "./Components/Nav";
 import AnimeState from "./Context/AnimeState";
-
+import AllAnime from "./Components/AllAnime";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <AnimeState>
-        <Nav/>
-          <Home></Home>
-      </AnimeState>
+        <AnimeState>
+      <Router>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Home></Home>} /> 
+            <Route exact path="/All" element={<AllAnime></AllAnime>} /> 
+          </Routes>
+      </Router>
+        </AnimeState>
     </>
   );
 }
---
+++ src/Components/AllAnime.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import AnimeContext from "../Context/Animecontext";
+import { useEffect } from "react";
+const AllAnime = () => {
+  const context = useContext(AnimeContext);
+  const { Anime, GetAllAnime } = context;
+  useEffect(() => {
+    GetAllAnime();
+    // eslint-disable-next-line
+  }, []);
+  return <>
+    {Anime.map((AllAnimes) => {
+    return <>1</>
+  })}
+  </>;
+};
+
+export default AllAnime;
